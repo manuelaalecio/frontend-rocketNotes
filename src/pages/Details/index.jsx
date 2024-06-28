5e2c189f0eb4f46b99f1395523bad4d3ffddf25c
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-28 15:06:49 -0300

    feat: create tag component and add on details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -17,6 +18,11 @@ export function Details() {
             <a href="#">www.rocketseat.com.br</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title={"express"} />
+        <Tag title={"nodejs"} />
       </Section>
 
       <Button title="Voltar" />
