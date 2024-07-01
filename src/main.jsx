3d9464909d72053b424477a679f6eb5ccc764c43
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-01 09:58:23 -0300

    feat: grid distribution on home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GlobalStyles from "./styles/global";
-import { Details } from "./pages/Details/";
 import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
 
 import theme from "./styles/theme";
+
+import { Home } from "./pages/Home/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 );
