33fbab9cbfd5a2169e1ba0078fd273f1564cf4e1
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-01 10:22:44 -0300

    feat: add style to menu

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,33 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
   return (
     <Container>
       <Brand>
-        <h1>Rocketnotes</h1>
+        <h1>RocketNotes</h1>
       </Brand>
       <Header />
 
-      <Menu></Menu>
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
+      </Menu>
 
       <Search></Search>
 
       <Content></Content>
 
-      <NewNote>
-        
-      </NewNote>
+      <NewNote></NewNote>
     </Container>
   );
 }
