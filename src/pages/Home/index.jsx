6e7f0e8e1dece7c00e141d3ed7d96069399b4d06
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-06 11:06:18 -0300

    feat: create Input component, and add on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiSearch } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
@@ -24,7 +25,9 @@ export function Home() {
         </li>
       </Menu>
 
-      <Search></Search>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+      </Search>
 
       <Content></Content>
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -46,7 +46,7 @@ export const Menu = styled.ul`
 `;
 export const Search = styled.div`
   grid-area: search;
-  background-color: violet;
+  padding: 64px 64px 0;
 `;
 export const Content = styled.div`
   grid-area: content;
