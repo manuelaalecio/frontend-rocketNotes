d63817c2d8f7cb566875b97d9d70319cc384b5d4
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-06 10:51:55 -0300

    feat: create home button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { FiPlus } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
@@ -27,7 +28,10 @@ export function Home() {
 
       <Content></Content>
 
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus />
+        Criar Nota
+      </NewNote>
     </Container>
   );
 }
