093cac08d5db85aa35ee643253dedd47d581a9f1
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-15 13:25:34 -0300

    feat: create page sign up and styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,13 @@ import GlobalStyles from "./styles/global";
 
 import theme from "./styles/theme";
 
-import { Home } from "./pages/Home/";
-import { SingIn } from "./pages/SingIn";
+import { SingUp } from "./pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SingIn />
+      <SingUp />
     </ThemeProvider>
   </React.StrictMode>
 );
