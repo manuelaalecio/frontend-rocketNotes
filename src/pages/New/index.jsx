6adbd2390d8a5c648cd4da8ad4177ba7cfa8fef0
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-15 18:19:52 -0300

    feat: add link from react router dom to app routes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -5,6 +5,8 @@ import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
 import { Button } from "../../components/Button";
 
+import { Link } from "react-router-dom";
+
 import { Container, Form } from "./styles";
 
 export function New() {
@@ -16,7 +18,7 @@ export function New() {
         <Form>
           <header>
             <h1>Criar Nota</h1>
-            <a href="/">Voltar</a>
+            <Link to="/">Voltar</Link>
           </header>
 
           <Input placeholder="Título" />
