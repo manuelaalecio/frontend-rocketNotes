ed3ad038eb8e5db10de083dad5d229ff9437d261
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-15 16:56:06 -0300

    feat: create user page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from "./styles/global";
 
 import theme from "./styles/theme";
 
-import { SingUp } from "./pages/SignUp";
+import { Profile } from "./pages/Profile";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SingUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,37 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Avatar } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Avatar>
+        <img src="https://github.com/manuelaalecio.png" alt="Foto do usuário" />
+
+        <label htmlFor="avatar">
+          <FiCamera />
+
+          <input id="avatar" type="file" />
+        </label>
+      </Avatar>
+
+      <Form>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="Email" type="email" icon={FiMail} />
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova Senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
