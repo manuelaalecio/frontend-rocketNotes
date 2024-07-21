e64fe0fde40ff73bc9e4b867027005fac1dc8faf
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-21 17:13:56 -0300

    feat: create auth sing in funcion

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,14 +1,30 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+
+import { api } from "../services/api";
 
 export const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
+  const [data, setData] = useState({});
+
+  async function singIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+      const { token, user } = response.data;
+
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({ token, user });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível realizar o login");
+      }
+    }
+  }
+
   return (
-    <AuthContext.Provider
-      value={{ Name: "Manuela", email: "manuela@email.com" }}
-    >
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ singIn, user: data.user }}>{children}</AuthContext.Provider>
   );
 }
 
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
+
+import { useAuth } from "../../hooks/auth";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -7,6 +11,15 @@ import { Button } from "../../components/Button";
 import { Container, Form, Background } from "./styles";
 
 export function SingIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { singIn } = useAuth();
+
+  function handleSingIn() {
+    singIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
@@ -15,10 +28,20 @@ export function SingIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="email" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSingIn} />
 
         <Link to={"/register"} href="#">
           Criar conta
