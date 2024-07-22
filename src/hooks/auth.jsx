94d01e49c0ba002e9c5f8f1c024994201e5c587d
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-22 16:14:58 -0300

    fix: updating to the latest axios settings

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -15,7 +15,7 @@ export function AuthProvider({ children }) {
       localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
       localStorage.setItem("@rocketnotes:token", token);
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       setData({ token, user });
     } catch (error) {
       if (error.response) {
@@ -38,7 +38,7 @@ export function AuthProvider({ children }) {
     const user = localStorage.getItem("@rocketnotes:user");
 
     if (token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
       setData({ token, user: JSON.parse(user) });
     }
